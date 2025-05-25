22eb5308520b48123bc70c5d5861d9f02cab473d
Philip Isakson <isaksonp@gmail.com>
2025-05-25 13:27:34 +0200

    Initial Commit

---
+++ Satin/SatinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatinApp.swift
+//  Satin
+//
+//  Created by Philip Isakson on 2025-05-25.
+//
+
+import SwiftUI
+
+@main
+struct SatinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
